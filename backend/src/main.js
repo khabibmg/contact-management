40a0930a75f828a70029f1733dd50a1ddc973d9b
khabibmg <mochkhabib24@gmail.com>
2026-09-15 22:45:32 +0700

    fix(backend): wrap web.listen and export express app for vercel

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,9 @@
-import {web} from "./application/web.js";
-import {logger} from "./application/logging.js";
+import { web } from './application/web.js'
+import { logger } from './application/logging.js'
+export default web
 
-web.listen(3000, () => {
-    logger.info("App start");
-});
+if (process.env.NODE_ENV !== 'production') {
+  web.listen(3000, () => {
+    logger.info('App start on port 3000')
+  })
+}
