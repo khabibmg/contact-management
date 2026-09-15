263496c2caab0b02a60701a80f1d71aa965de63e
khabibmg <mochkhabib24@gmail.com>
2026-09-16 01:02:24 +0700

    fix(backend): update vercel serverless entrypoint and config

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,3 @@
-import { web } from '../src/application/web.js'
+import app from '../src/application/web.js'
 
-export default function handler(req, res) {
-  // Tangani preflight OPTIONS secara langsung untuk mencegah Vercel redirect / blokir CORS
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    return res.status(200).end()
-  }
-
-  return web(req, res)
-}
+export default app
